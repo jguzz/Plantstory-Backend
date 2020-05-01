92d1ee91e9e2df2564f8780ec62254e09e45c9dd
github username <github email address>
2020-05-01 16:24:56 -0400

    first setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_153656) do
+ActiveRecord::Schema.define(version: 2020_05_01_191935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_153656) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "profile_picture"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Plant.destroy_all
+Post.destroy_all
+Photo.destroy_all
+Comment.destroy_all
+Follow.destroy_all
+
+joey = User.create(username: "joeyG", name:"joey", password_digest: "123", bio: "plant-lover from queens!", profile_picture: "https://vignette.wikia.nocookie.net/rare/images/2/25/Smash_ultimate_banjo.jpg/revision/latest?cb=20190612041147")
+jake = User.create(username: "jakeP", name: "jake", password_digest: "123", bio: "newbie plant lover", profile_picture: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F5c%2F2e%2F90%2F5c2e90686ecc230c903db6e8d2a2fb5e.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.co.uk%2Fpin%2F767160117753837436%2F&tbnid=1AZN0w2MJyayfM&vet=12ahUKEwimtor5sZPpAhXVheAKHXZXA9IQMygJegUIARCmAg..i&docid=UwDvz2I8KpX1ZM&w=1790&h=2048&q=ranma%201%2F2&hl=en&ved=2ahUKEwimtor5sZPpAhXVheAKHXZXA9IQMygJegUIARCmAg")
+
+plant1 = Plant.create(common_name: "snake plant", latin_name: "sansaveria")
+plant2 = Plant.create(common_name: "swiss-cheese plant", latin_name: "monstera")
+
+photo1 = Photo.create(img: "https://www.burpee.com/dw/image/v2/ABAQ_PRD/on/demandware.static/-/Sites-masterCatalog_Burpee/default/dw4299e75a/Images/Product%20Images/prod500591/prod500591.jpg?sw=322&sh=380&sm=fit")
+photo2 = Photo.create(img: "https://i.pinimg.com/originals/e6/98/b8/e698b81ad2f7f12f32b69d726a853263.jpg")
+
+post1 = Post.create(likes: 0, user_id: 1, plant_id: 1, photo_id: 1, comment_id: nil)
+post2 = Post.create(likes: 2, user_id: 2, plant_id: 2, photo_id: 2, comment_id: nil)
