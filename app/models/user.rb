3920962e8992be2235a6f004c3944a8d2ffea0e5
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-04 12:17:56 -0400

    added gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_many :posts
-	has_many :comments, through: :post
+	has_many :comments, through: :posts
+	has_many :plants, through: :posts
 	has_many :followed_users, foreign_key: :follower_id, class_name:'Follow'
 	has_many :followees, through: :followed_users
 	has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
